55df1334390ed612afe0b678794d4f3895124c03
Jerzy Dziewierz <jerzy.dziewierz@strath.ac.uk>
2017-04-30 09:25:29 +0100

    adding documentation using Sphinx

--- python/cueBeamCore.py
+++ python/cueBeamCore.py
@@ -5,25 +5,40 @@ import matplotlib.pyplot as plt
 
 class CueBeamSolver:
     """ 
-    This is the core of the cueBeam
-    properties:
-    * wavenumber(float) - the enviroment property
-    * elements(list of TxElement) - description of the transmitters
-    * rxPlane(single item of class RxPlane()) - describes the regular sampling plane
+    This is the core of the cueBeam.
+    
+    **Essential fields that You need to set:**
+    
+    wavenumber: float
+        The enviroment property, note that wavenumber = 1/wavelength in that enviroment
+    elements: list of TxElement
+        description of the transmitters
+    rxPlane: single item of class RxPlane()
+        describes the regular sampling plane
+    
+    
+    
     
     """
 
     wavenumber = 1.0 / 1.0e-3  # wavelength = 1/wavenumber; so wavenumber = 1/wavelength
-    "property of the environment: the wavenumber"
+    """Property of the environment: the wavenumber."""
 
     class TxElement:
-        x = 0.0
-        y = 0.0
-        z = 0.0
-        amplitude = 1.0
-        phase = 0.0
+        """Describes a single transmitting monopole."""
 
-        def __init__(self, x=0.0, y=0.0, z=0.0, amplitude=1.0, phase=0.0):
+        x = 0.0
+        """X-location of the monopole."""
+        y = 0.0
+        """Y-location of the monopole."""
+        z = 0.0
+        """Z-location of the monopole."""
+        amplitude = 1.0
+        """Amplitude of the vibration of the monopole."""
+        phase = 0.0
+        """Relative phase of the monopole (relative to other monopoles)"""
+
+        def __init__(self, x:float=0.0, y:float=0.0, z:float=0.0, amplitude:float=1.0, phase:float=0.0):
             self.x = x
             self.y = y
             self.z = z
@@ -38,23 +53,35 @@ class CueBeamSolver:
                 TxElement(0.0, 0.0,  10.0e-3, 1.0, 0.0),
                 TxElement(0.0, 0.0,  15.0e-3, 1.0, 0.0)
                 ]
+    """List of monopoles.
+    
+    List of TxElement() - the list of monopoles emitting energy into the medium"""
 
-    # for the planar sampling version:
-    # the original Matlab version: img_xz = cueBeam_xz(tx',k,x0,y0,z0,nx,ny,nz,dx,dy,dz);
     class RxPlane:
         """describes a rectangular XZ section of the plane - and points of sampling(calculation) of the pressure"""
         x0 = -0.01e-3  # needs to be a little bit off plane to avoid division by zer
+        "x-origin of the measurement plane"
         y0 = -1.0e-3
+        "y-origin of the measurement plane"
         z0 = +0.0e-3
+        "z-origin of the measurement plane"
         dx = +1.0e-3
+        "x-step between the points in the measurement plane"
         dy = +1.0e-3
+        "y-step between the points in the measurement plane"
         dz = +1.0e-3
+        "z-step between the points in the measurement plane"
         nx = 1
+        "number of measurement points along x-axis"
         ny = 240
+        "number of measurement points along y-axis"
         nz = 160
+        "number of measurement points along z-axis"
         pressurefield = numpy.zeros((ny, nz), numpy.complex)
+        "the numpy array to store the result of measurement. Note: complex numbers"
 
         def __init__(self, x0=-5.0e-3, y0=-5.0e-3, z0=0.0, dx=0.1e-3, dy=0.1e-3, dz=0.1e-3, nx=1, ny=10, nz=10):
+            "Creates the new instance of RxPlane"
             self.x0 = x0
             self.y0 = y0
             self.z0 = z0
@@ -72,14 +99,31 @@ class CueBeamSolver:
             self.clear_pressurefield()
 
         def verify_plane_endpoints(self):
+            "returns x,y,z location of the corner oposite to the origin corner"
             return [self.x0 + self.nx * self.dx, self.y0 + self.ny * self.dy, self.z0 + self.nz * self.dz]
 
         def clear_pressurefield(self):
+            "empties the result"
             self.pressurefield = numpy.zeros((self.ny, self.nz), numpy.complex)
 
     rxPlane = RxPlane()
+    "contains the description/location of the measurement points"
 
     def beamsim(self):
+        """Execute the pressure calculation.
+        
+        Executes the calculation using settings stored in elements, rxPlane and wavenumber.
+        
+        Parameters
+        ----------
+        None. Takes input from the elements, rxPlane and wavenumber.
+        
+        Returns
+        -------
+        None. The result is stored in .pressurefield field
+        
+        
+        """
 
         # note: there is only a single plane implemented. This is for compatibility with the old Matlab-Cuda version.
         # Essentially, there should be no problem in extending this to 3D computation as needed.
@@ -108,6 +152,10 @@ class CueBeamSolver:
                 # end beamsim
 
     def do_plot(self):
+        """Makes an example plot.
+        
+         It is configured for the default data. Compatible with Jupyter"""
+
         hfig = plt.figure(num=1, figsize=(8, 6), dpi=90, facecolor='white', edgecolor='black')
         imgplot = plt.imshow(
             X=numpy.real(self.rxPlane.pressurefield),
@@ -118,11 +166,9 @@ class CueBeamSolver:
             interpolation="spline36",
             clim=(-7.0, 7.0),
             origin="lower")
+        # end imshow
         plt.set_cmap("PRGn")
         plt.xlabel("z-axis[m]")
         plt.ylabel("y-axis[m]")
         plt.show()
 
-        # q=cueBeamCore.CueBeamSolver()
-        # q.rxPlane.clear_pressurefield()
-        # q.beamsim()
