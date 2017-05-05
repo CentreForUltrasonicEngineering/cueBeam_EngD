6173fc479fbc1b616af8b785fcd033323b55b3a6
Jerzy Dziewierz <jerzy.dziewierz@strath.ac.uk>
2017-05-05 10:50:28 +0100

    testing 2

--- python/cueBeamCore.py
+++ python/cueBeamCore.py
@@ -1,6 +1,8 @@
 import math
 import numpy
 import matplotlib.pyplot as plt
+import time
+import copy
 
 
 class CueBeamSolver:
@@ -15,10 +17,7 @@ class CueBeamSolver:
         description of the transmitters
     rxPlane: single item of class RxPlane()
         describes the regular sampling plane
-    
-    
-    
-    
+
     """
 
     wavenumber = 1.0 / 1.0e-3  # wavelength = 1/wavenumber; so wavenumber = 1/wavelength
@@ -38,20 +37,20 @@ class CueBeamSolver:
         phase = 0.0
         """Relative phase of the monopole (relative to other monopoles)"""
 
-        def __init__(self, x:float=0.0, y:float=0.0, z:float=0.0, amplitude:float=1.0, phase:float=0.0):
+        def __init__(self, x: float=0.0, y: float=0.0, z: float=0.0, amplitude: float=1.0, phase: float=0.0):
             self.x = x
             self.y = y
             self.z = z
             self.amplitude = amplitude
             self.phase = phase
 
-    elements = [TxElement(0.0, 0.0, -15.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0, -10.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0,  -5.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0,   0.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0,   5.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0,  10.0e-3, 1.0, 0.0),
-                TxElement(0.0, 0.0,  15.0e-3, 1.0, 0.0)
+    elements = [TxElement(0.0,  3.0e-3, -15.0e-3, 1.0, 0.0),
+                TxElement(0.0,  2.0e-3, -10.0e-3, 1.0, 0.0),
+                TxElement(0.0,  1.0e-3,  -5.0e-3, 1.0, 0.0),
+                TxElement(0.0,  0.0e-3,   0.0e-3, 1.0, 0.0),
+                TxElement(0.0, -1.0e-3,   5.0e-3, 1.0, 0.0),
+                TxElement(0.0, -2.0e-3,  10.0e-3, 1.0, 0.0),
+                TxElement(0.0, -3.0e-3,  15.0e-3, 1.0, 0.0)
                 ]
     """List of monopoles.
     
@@ -61,13 +60,13 @@ class CueBeamSolver:
         """describes a rectangular XZ section of the plane - and points of sampling(calculation) of the pressure"""
         x0 = -0.01e-3  # needs to be a little bit off plane to avoid division by zer
         "x-origin of the measurement plane"
-        y0 = -1.0e-3
+        y0 =  1.0e-3
         "y-origin of the measurement plane"
         z0 = +0.0e-3
         "z-origin of the measurement plane"
-        dx = +1.0e-3
+        dx = +0.5e-3
         "x-step between the points in the measurement plane"
-        dy = +1.0e-3
+        dy = +0.5e-3
         "y-step between the points in the measurement plane"
         dz = +1.0e-3
         "z-step between the points in the measurement plane"
@@ -80,7 +79,7 @@ class CueBeamSolver:
         pressurefield = numpy.zeros((ny, nz), numpy.complex)
         "the numpy array to store the result of measurement. Note: complex numbers"
 
-        def __init__(self, x0=-5.0e-3, y0=-5.0e-3, z0=0.0, dx=0.1e-3, dy=0.1e-3, dz=0.1e-3, nx=1, ny=10, nz=10):
+        def __init__(self, x0=-0.01e-3, y0=1.0e-3, z0=0.0e-3, dx=0.5e-3, dy=0.5e-3, dz=0.5e-3, nx=1, ny=240, nz=160):
             "Creates the new instance of RxPlane"
             self.x0 = x0
             self.y0 = y0
@@ -109,6 +108,16 @@ class CueBeamSolver:
     rxPlane = RxPlane()
     "contains the description/location of the measurement points"
 
+    def beamsim_timed(self):
+        t1 = time.clock()
+        self.beamsim()
+        t2 = time.clock()
+        dt = t2 - t1
+        rays_per_second = self.get_ray_count() / dt
+        print("got {:0.2f} M rays per second".format(rays_per_second/1e6))
+        return rays_per_second
+
+
     def beamsim(self):
         """Execute the pressure calculation.
         
@@ -127,31 +136,47 @@ class CueBeamSolver:
 
         # note: there is only a single plane implemented. This is for compatibility with the old Matlab-Cuda version.
         # Essentially, there should be no problem in extending this to 3D computation as needed.
+        local_rx_plane = self.rxPlane
+        local_elements = self.elements
+        local_wavenumber = self.wavenumber
+
         ix = 0
-        for iz in range(self.rxPlane.nz):
-            for iy in range(self.rxPlane.ny):
+        for iz in range(local_rx_plane.nz):
+            for iy in range(local_rx_plane.ny):
+                # initialize accumulator
                 pressure_re = 0.0
                 pressure_im = 0.0
-                pixel_x = self.rxPlane.x0 + ix * self.rxPlane.dx
-                pixel_y = self.rxPlane.y0 + iy * self.rxPlane.dy
-                pixel_z = self.rxPlane.z0 + iz * self.rxPlane.dz
-                for itx in range(len(self.elements)):
-                    ddx = (pixel_x - self.elements[itx].x)
-                    ddy = (pixel_y - self.elements[itx].y)
-                    ddz = (pixel_z - self.elements[itx].z)
+                # calculate the XYZ location of the transmitter
+                pixel_x = local_rx_plane.x0 + ix * local_rx_plane.dx
+                pixel_y = local_rx_plane.y0 + iy * local_rx_plane.dy
+                pixel_z = local_rx_plane.z0 + iz * local_rx_plane.dz
+                # for each element do:
+                for itx in range(len(local_elements)):
+                    # calculate distance between the transmitter element and receiver pixel
+                    ddx = (pixel_x - local_elements[itx].x)
+                    ddy = (pixel_y - local_elements[itx].y)
+                    ddz = (pixel_z - local_elements[itx].z)
                     distance = math.sqrt(ddx * ddx + ddy * ddy + ddz * ddz)
-                    kphase = - self.wavenumber * distance + self.elements[itx].phase
-                    kamplitude = self.elements[itx].amplitude / (2 * 3.14159 * distance)
-                    pressure_re += math.cos(kphase) * kamplitude
-                    pressure_im += math.sin(kphase) * kamplitude
-                # mem write
-                self.rxPlane.pressurefield[(iy, iz)] = pressure_re + 1.0j * pressure_im
+                    # based on distance and wavenumber, calculate the amplitude and phase
+                    k_phase = - local_wavenumber * distance + local_elements[itx].phase
+                    k_amplitude = local_elements[itx].amplitude / (2 * 3.14159 * distance)
+                    # increment the accumulator
+                    pressure_re += math.cos(k_phase) * k_amplitude
+                    pressure_im += math.sin(k_phase) * k_amplitude
+                # memory write
+                local_rx_plane.pressurefield[(iy, iz)] = pressure_re + 1.0j * pressure_im
 
                 # end for iy
-                # end for iz
-                # end beamsim
+            # end for iz
+        # store result to the object
+        self.rxPlane = copy.copy(local_rx_plane)
+        # end beamsim
 
-    def do_plot(self):
+
+    def get_ray_count(self):
+        return self.rxPlane.nx * self.rxPlane.ny * self.rxPlane.nz * len(self.elements)
+
+    def do_plot_real(self):
         """Makes an example plot.
         
          It is configured for the default data. Compatible with Jupyter"""
@@ -164,11 +189,31 @@ class CueBeamSolver:
                 self.rxPlane.y0 + self.rxPlane.ny * self.rxPlane.dy, self.rxPlane.y0
             ),
             interpolation="spline36",
-            clim=(-7.0, 7.0),
+            clim=(-8.0, 8.0),
             origin="lower")
         # end imshow
-        plt.set_cmap("PRGn")
+        plt.set_cmap("PRGn")  # purple-white-green color map
         plt.xlabel("z-axis[m]")
         plt.ylabel("y-axis[m]")
         plt.show()
 
+    def do_plot_abs(self):
+        """Makes an example plot.
+
+         It is configured for the default data. Compatible with Jupyter"""
+
+        hfig = plt.figure(num=1, figsize=(8, 6), dpi=90, facecolor='white', edgecolor='black')
+        imgplot = plt.imshow(
+            X=numpy.abs(self.rxPlane.pressurefield),
+            extent=(
+                self.rxPlane.z0, self.rxPlane.z0 + self.rxPlane.nz * self.rxPlane.dz,
+                self.rxPlane.y0 + self.rxPlane.ny * self.rxPlane.dy, self.rxPlane.y0
+            ),
+            interpolation="spline36",
+            clim=(0, 8.0),
+            origin="lower")
+        # end imshow
+        plt.set_cmap("plasma") # black-to-yellow color map
+        plt.xlabel("z-axis[m]")
+        plt.ylabel("y-axis[m]")
+        plt.show()
