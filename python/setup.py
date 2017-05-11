3dd65697621e08d0cee5b20572b595e86b71a4e4
Jerzy Dziewierz <jerzy.dziewierz@strath.ac.uk>
2017-05-11 12:16:23 +0100

    added documentation stub

--- python/setup.py
+++ python/setup.py
@@ -8,8 +8,5 @@ setup(
     license='Creative commons BY-NC-SA',
     author='Jerzy Dziewierz',
     author_email='jerzy.dziewierz@strath.ac.uk',
-    description='Simple acoustic interference and beam forming simulation based on Huygen'
-s
-principle
-'
+    description='Simple acoustic interference and beam forming simulation based on Huygen''s principle'
 )
