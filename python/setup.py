890d9c42e33b9b37c0dc51ceb2a8050159bb0e02
Jerzy Dziewierz <jerzy.dziewierz@strath.ac.uk>
2017-05-11 12:15:53 +0100

    moving files around - clean-up

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='cueBeam',
+    version='0.0.1',
+    packages=[''],
+    url='no-url-yet',
+    license='Creative commons BY-NC-SA',
+    author='Jerzy Dziewierz',
+    author_email='jerzy.dziewierz@strath.ac.uk',
+    description='Simple acoustic interference and beam forming simulation based on Huygen'
+s
+principle
+'
+)
