4f42e1f10059bc57520d0761ecd3a9f486107909
Jerzy Dziewierz <mib07150@EEE-ULTRA26>
2017-06-05 17:51:28 +0100

    making the bugsnag to install automatically should save me some clicking around

--- python/bugcatcher.py
+++ python/bugcatcher.py
@@ -1,5 +1,17 @@
 # notify bugsnag that i am imported
-import bugsnag
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('bugsnag')
+
 from bugsnag.handlers import BugsnagHandler
 
 import logging
